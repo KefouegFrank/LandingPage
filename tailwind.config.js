245178939d0f1e3f11b5c11cb3d42b6d54e5410e
unknown <KefouegFrank>
2024-03-13 21:47:06 +0100

    successfull completion of task

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    screens:{
-
-    }, 
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "144px",
+    },
+    extend: {
+      colors: {
+        BrightRed: "hsl(12, 88%, 59%)",
+        BrightRedLight: "hsl(12, 88%, 65%)",
+        BrightRedLighter: "hsl(12, 88%, 90%)",
+        DarkBlue: "hsl(228, 39%, 23%)",
+        DarkGrayishBlue: "hsl(227, 12%, 61%)",
+        VeryDarkBlue: "hsl(233, 12%, 13%)",
+        VeryPaleRed: "hsl(13, 100%, 96%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
